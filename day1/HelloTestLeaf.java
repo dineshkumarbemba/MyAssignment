4b69a9bd772f291a624e0a69033756db9834617a
dineshkumarbemba <dineshkumarbemba27@gmail.com>
2025-07-24 20:03:20 +0530

    Add files via upload
    week1day1

---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class HelloTestLeaf {
+	int no=32;
+	public void firstMethod() {
+		// TODO Auto-generated method stub
+		System.out.println(no);
+	}
+public static void main(String[] args) {
+
+HelloTestLeaf fs = new HelloTestLeaf();
+fs.firstMethod();
+}
+}
